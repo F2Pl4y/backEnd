011983e301b0c027d8b4b0ea2639d44748a9b1dc
F2Pl4y <fernando.hurtado600@hotmail.com>
2022-06-29 13:13:20 -0500

    save

--- util/Config.py
+++ util/Config.py
@@ -1,7 +1,7 @@
 # Se tendra toda la configuacion acerda con la base de datos
 class Config:
     MYSQL_HOST = "remotemysql.com"
-    MYSQL_PORT = 3306
+    # MYSQL_PORT = 3306
     MYSQL_USER = "vEsOBsawtL"
     MYSQL_PASS = "yayC7Tpv7u"
     MYSQL_DB = "vEsOBsawtL"
